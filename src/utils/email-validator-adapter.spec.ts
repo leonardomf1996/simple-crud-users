ba3fdacf9b2661bbb77f40662561371e320ccd14
Leonardo <leonardomf1996@gmail.com>
2022-10-17 23:51:03 -0300

    test: ensure EmailValidator return true if returns true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -20,4 +20,10 @@ describe('EmailValidator', () => {
       const isValid = sut.isValid('invalid_email@mail.com');
       expect(isValid).toBe(false);
    });
+
+   test('should return true if validator returns true', () => {
+      const sut = makeSut();
+      const isValid = sut.isValid('valid_email@mail.com');
+      expect(isValid).toBe(true);
+   });
 });
